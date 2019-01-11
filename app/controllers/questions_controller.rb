50ba3479c3fcf0b7e5066e2cd236e084c7bc9082
Daisuke Minagawa <iamworkingwell@gmail.com>
2019-01-11 09:17:28 +0900

    second update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,7 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:edit, :update, :destroy]
   def index
     @questions = Question.all
-  end
-
-  def edit
   end
 
   def new
@@ -13,22 +11,38 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(param_question)
     if @question.save #saveの前に、validateが走り、エラーだと@questionに情報が入れられる
+      redirect_to root_path, notice: 'Succeed'
     else
       flash[:error] = @question.errors.full_messages
+      render :new
     end
   end
 
   def show
   end
 
+  def edit
+  end
+
   def update
+    if @question.update(param_question)
+      flash[:succeed] = 'Update is succeed'
+      redirect_to root_path
+    end
   end
 
   def destroy
+    @question.destroy
+    redirect_to root_path, notice: 'Delete is Success'
   end
 
   private
+
   def param_question
     params.require(:question).permit(:title, :content)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
